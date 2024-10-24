1492c7a6d72f7af0808f86b0c2b5b898b0b24f1a
Bill Clark <saikou@restdnn>
2024-10-24 11:44:07 -0400

    Another tweak with the ve_prompt

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='virtualenv-better-bash',
-    version='0.3.1',
+    version='0.3.2',
     description='Add in custom modifications to virtualenv',
     author='Bill Clark',
     packages=find_packages(),
